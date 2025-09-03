8d35f464548c2b0938cf46a01b42c61de6def2a3
DBDA11 <dbda11you@gmail.com>
2025-09-03 17:42:39 +0530

    fixed bug 2

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-print("hello-testing git branching")
+print("hello-testing git branching - Added by developer 2")
 print("main branch")
+printf("This is from developer 2")
+printf("This is a new line by dev2")
+
