deb9bee6af5e8ea637436ba5fb424b10c763562c
dbda11 <dbda11you@gmail.com>
2025-09-04 10:36:56 +0530

    edited file

--- hello.py
+++ hello.py
@@ -1,6 +1,2 @@
-print("hello-testing git branching - Added by developer 2")
-print("main branch")
-printf("This is from developer 2")
-printf("This is a new line by dev2")
-
+print("edited the file from a github cloned repo")
 
